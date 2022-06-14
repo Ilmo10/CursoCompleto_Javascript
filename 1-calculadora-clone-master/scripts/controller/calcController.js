1a5c6ea54414310d30cffa2edcabc9076d0d7935
Ilmo10 <necok_cley@hotmail.com>
2022-06-14 15:15:40 -0300

    criando método inialize

--- 1-calculadora-clone-master/scripts/controller/calcController.js
+++ 1-calculadora-clone-master/scripts/controller/calcController.js
@@ -1,44 +1,47 @@
-    // variavel dentro de class, são chamadas de ATRIBUTOS
-     //function dentro de class,são chamadas de MÉTODOS
-     //sempre que precisar chamar uma class, precisa ser feito um OBJ, que essa ação é chamada de INSTANCIA
-
-     // this. é utilizado para chamar os atributos em qualquer lugar da class. 
-
-     ////acho que foi
-
 class CalcController {
 
     constructor(){
-        this._displayCalc = '0';
+
+        this._displayCalc = "0";
         this._currentDate;
-        this.initialize()
+        this.initialize();
+
     }
 
     initialize(){
-        
-        let displayCalcEl = document.querySelector('#display');
-        let dateEl = document.querySelector('#data');
-        let timeEl = document.querySelector('#hora');
 
-        displayCalcEl.innerHTML = "3456";
-        dateEl.innerHTML = "08/06/2022";
-        timeEl.innerHTML = "15:25";
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "15/04/2021";
+        timeEl.innerHTML = "11:14";
+
     }
 
-    get displayCalc (){
+    get displayCalc(){
+
         return this._displayCalc;
+
     }
 
     set displayCalc(valor){
+
         this._displayCalc = valor;
+
     }
 
-    get currentDate (){
-        return this.currentDate;
+    get currentDate(){
+
+        return this._currentDate;
+
     }
 
     set currentDate(valor){
-        this.currentDate = valor;
+
+        this._currentDate = valor;
+
     }
 
 }
